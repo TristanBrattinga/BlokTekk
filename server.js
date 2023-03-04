610477f6e8ccba25c78ddd15053f7ded9fcb025c
Tristan Brattinga <tristan.brattinga@gmail.com>
2023-03-04 11:06:17 +0100

    Installed EJS and added views and partials

--- server.js
+++ server.js
@@ -1,28 +1,48 @@
-// const express = require("express");
-// const app = express();
-// const port = 8080;
-
-// app.get("/", (req, res) => {
-//   res.send("<h1>Hello World!</h1>");
-// });
-
-// app.listen(port, function () {
-//   console.log(`Example app listening on port ${port}!`);
-// });
-
-// ABOVE EXAMPLE FROM EXPRESSJS.COM //
-
-
 const express = require('express');
+const app = express();
+const port = 8080;
 
-express()
-  .get('/', onhome)
-  .listen(8080, function () {
-    console.log(`My new first server hosted on port 8080!`);
-  })
 
-function onhome(req, res) {
-  res.send('<h1>Hello World!</h1>');
-}
+app.use('/public', express.static('public'));
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-// HERE OWN VERSION //
+
+app.get('/', (req, res) => {
+  res.render('index', {message: 'Hi, how are you doing?'})
+});
+
+app.get('/register', (req, res) => {
+  res.render('register', {welcomemessage: 'Hello Tristan, I hope u have a beautiful day!'})
+});
+
+app.get('/login', (req, res) => {
+  res.render('login', {check: 'Hoi DiT WErkt Nu wEl'})
+});
+
+app.get('/*', (req, res) => {
+  res.status(404).send('Sorry, the page you are looking for was not found.');
+});
+
+app.listen(port, () => {
+  console.log(`My new first server hosted on port ${port}!`)
+});
+
+
+
+// Here we'll make an object to shove into the homepage view
+
+var userList = [
+  {
+    Name: 'Tristan',
+    Age: '23',
+    Gender: 'Male',
+    Genres: ["Drum & Bass", "Rap", "Old School Hip Hop"]
+  },
+  {
+    Name: 'Serena',
+    Age: '24',
+    Gender: 'Female',
+    Genres: ["Pop", "Drum & Bass", "Rap", "Tech-House"]
+  }
+];
