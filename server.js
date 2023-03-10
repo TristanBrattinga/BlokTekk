acffcd568549ed18cd4cd72a2b0eaa0227273964
Tristan Brattinga <tristan.brattinga@gmail.com>
2023-03-10 14:05:37 +0100

    MongoDB database made and connected

--- server.js
+++ server.js
@@ -1,33 +1,18 @@
 const express = require('express');
+const chalk = require('chalk');
 const app = express();
 const port = 8080;
 
-app
+console.log(chalk.blue('Hello'),chalk.magenta('World!')); // DIT IS EEN CHECK VOOR CHALK
+
 app.use('/public', express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-
-app.get('/', (req, res) => {
-  res.render('index', {message: 'Hi, how are you doing?'})
-});
-
-app.get('/register', (req, res) => {
-  res.render('register', {welcomemessage: 'Hello Tristan, I hope u have a beautiful day!'})
-});
-
-app.get('/login', (req, res) => {
-  res.render('login', {check: 'Hoi DiT WErkt Nu wEl'})
-});
-
-
-app.listen(port, () => {
-  console.log(`My new first server hosted on port ${port}!`)
-});
-
-
-
-// Here we'll make an object to shove into the homepage view
+// Here I'll make an object to shove into the homepage view
 
 const users = [
   {
@@ -44,6 +29,12 @@ const users = [
   }
 ];
 
+const genres = ["Drum & Bass", "Pop", "Rap", "Tech-House", "Old School Hip Hop"];
+
+app.get('/', (req, res) => {
+  res.render('index', {message: 'Hi, how are you doing?'})
+});
+
 app.get('/users', (req, res) => {
   res.render('userlist', {title: 'All users', users})
 });
@@ -53,7 +44,43 @@ app.get('/users/:userName', (req, res) => {
   res.render('userdetails', {title: `User details for ${user.name}`, user})
 });
 
+app.get('/register', (req, res) => {
+  res.render('register', {title: 'Sign up here', genres})
+});
+
+app.post('/users', (req, res) => {
+  let user = {name: req.body.name, age: req.body.age, gender: req.body.gender, genres: req.body.genres};
+  console.log(req.body.genres);
+  users.push(user);
+  res.render('userlist', {title: 'Succesfully registered user', users})
+});
+
 
 app.get('/*', (req, res) => {
   res.status(404).send('Sorry, the page you are looking for was not found.');
 });
+
+async function connectDB(){
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://Tristan:8pntmHWnsAqtyDwr@clustertristoni.jpag5is.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+try {
+  console.log('awaiting connection');
+  await client.connect();
+  console.log('connected');
+  db = client.db('Testdb');
+}
+catch(error){
+  console.error(error);
+  throw error;
+}
+};
+
+
+app.listen(port, async () => {
+  console.log(chalk.green(`My new first server hosted on port ${port}!`));
+  let databaseConnection = await connectDB();
+  let theData = await db.collection('collection1').find({ name : 'Quinten'}).toArray();
+  console.log(theData);
+});
+
