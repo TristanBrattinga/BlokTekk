cd8f4dca6dae38536650d9d0a3e112014014369b
Tristan Brattinga <tristan.brattinga@gmail.com>
2023-03-13 22:29:45 +0100

    Worked on CSS

---
+++ public/js/script.js
@@ -0,0 +1,7 @@
+const burgerBtn = document.querySelector("header nav button");
+const mobileNav = document.querySelector("header nav ul");
+
+burgerBtn.addEventListener("click", () => {
+    burgerBtn.classList.toggle("showMenu");
+    mobileNav.classList.toggle("showMenu");
+});
--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const chalk = require('chalk');
 const app = express();
 const port = 8080;
 require('dotenv').config();
-console.log(process.env);
 
 console.log(chalk.blue('Hello'),chalk.magenta('World!')); // DIT IS EEN CHECK VOOR CHALK
 
@@ -13,23 +12,6 @@ app.use(express.urlencoded({extended: true}));
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
-
-// Here I'll make an object to shove into the homepage view
-
-const users = [
-  {
-    name: 'Tristan',
-    age: '23',
-    gender: 'Male',
-    genres: ["Drum & Bass", "Rap", "Old School Hip Hop"]
-  },
-  {
-    name: 'Serena',
-    age: '24',
-    gender: 'Female',
-    genres: ["Pop", "Drum & Bass", "Rap", "Tech-House"]
-  }
-];
 
 const genres = ["Drum & Bass", "Pop", "Rap", "Tech-House", "Old School Hip Hop"];
 
@@ -47,16 +29,8 @@ app.get('/users/:userName', (req, res) => {
 });
 
 app.get('/register', (req, res) => {
-  res.render('register', {title: 'Sign up here', genres})
+  res.render('register', {title: 'Sign up here', genres});
 });
-
-app.post('/users', (req, res) => {
-  let user = {name: req.body.name, age: req.body.age, gender: req.body.gender, genres: req.body.genres};
-  console.log(req.body.genres);
-  users.push(user);
-  res.render('userlist', {title: 'Succesfully registered user', users})
-});
-
 
 app.get('/*', (req, res) => {
   res.status(404).send('Sorry, the page you are looking for was not found.');
@@ -83,6 +57,29 @@ app.listen(port, async () => {
   console.log(chalk.green(`My new first server hosted on port ${port}!`));
   let databaseConnection = await connectDB();
   let theData = await db.collection('collection1').find({}).toArray();
-  console.log(theData);
 });
 
+
+app.post('/userdetails', async (req, res) => {
+  connectDB();
+
+  const userdata = {
+    name: req.body.name,
+    age: req.body.age,
+    gender: req.body.gender,
+    genres: req.body.genres
+  };
+  db.collection('collection1').insertOne(userdata,function(err, collection)
+  {if (err) throw err;
+    console.log("succes");
+  })
+  res.send(userdata);
+});
+
+
+
+
+app.delete('/deleted'), async (req, res) => {
+  const userIdToDelete = {_id: req.body.id};
+  db.collection1.deleteOne()
+}
