3afd97f52f8dfae5155712c3ee77a2b670fa82bf
Tristan Brattinga <tristan.brattinga@gmail.com>
2023-03-15 18:23:30 +0100

    Added ESLint

--- public/js/script.js
+++ public/js/script.js
@@ -4,4 +4,6 @@ const mobileNav = document.querySelector("header nav ul");
 burgerBtn.addEventListener("click", () => {
     burgerBtn.classList.toggle("showMenu");
     mobileNav.classList.toggle("showMenu");
+
+
 });
--- server.js
+++ server.js
@@ -1,77 +1,109 @@
-const express = require('express');
-const chalk = require('chalk');
+const express = require("express");
+const chalk = require("chalk");
+// const bcrypt = require("bcrypt");
+const slug = require("slug");
 const app = express();
 const port = 8080;
-require('dotenv').config();
+require("dotenv").config();
 
-console.log(chalk.blue('Hello'),chalk.magenta('World!')); // DIT IS EEN CHECK VOOR CHALK
+console.log(chalk.blue("Hello"), chalk.magenta("World!")); // DIT IS EEN CHECK VOOR CHALK
 
-app.use('/public', express.static('public'));
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app
+  .use("/public", express.static("public"))
+  .use(express.json())
+  .use(express.urlencoded({ extended: true }))
+  .set("view engine", "ejs")
+  .set("views", "views");
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+const date = new Date().toLocaleDateString();
 
-const genres = ["Drum & Bass", "Pop", "Rap", "Tech-House", "Old School Hip Hop"];
+// const genres = ["Drum & Bass", "Pop", "Rap", "Tech-House", "Old School Hip Hop"];
 
-app.get('/', (req, res) => {
-  res.render('index', {message: 'Hi, how are you doing?'})
+// app.get("/users", (req, res) => {
+//   res.render("userlist", { title: "All users", users });
+// });
+
+// app.get("/users/:userName", (req, res) => {
+//   const user = users.find(user => user.name == req.params.userName);
+//   res.render("account");
+// });
+
+// HOMEPAGE
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
-app.get('/users', (req, res) => {
-  res.render('userlist', {title: 'All users', users})
+// REGISTER PAGE
+app.get("/register", (req, res) => {
+  res.render("register", { title: "Sign up here", date });
 });
 
-app.get('/users/:userName', (req, res) => {
-  const user = users.find(user => user.name == req.params.userName);
-  res.render('userdetails', {title: `User details for ${user.name}`, user})
+// LOGIN PAGE
+app.get("/login", (req, res) => {
+  res.render("login");
 });
 
-app.get('/register', (req, res) => {
-  res.render('register', {title: 'Sign up here', genres});
+// ACCOUNT PAGE
+app.get("/account/:userId/:slug", (req, res) => {
+  res.render("account");
 });
 
-app.get('/*', (req, res) => {
-  res.status(404).send('Sorry, the page you are looking for was not found.');
+// CONTACT PAGE
+app.get("/contact", (req, res) => {
+  res.render("contact");
 });
 
-async function connectDB(){
-  const { MongoClient, ServerApiVersion } = require('mongodb');
+// MONGODB CONNECTION
+
+async function connectDB () {
+  const { MongoClient, ServerApiVersion } = require("mongodb");
   const uri = process.env.DB_CONNECTION_STRING;
   const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
   try {
-    console.log('awaiting connection');
+    console.log("awaiting connection");
     await client.connect();
-    console.log('connected');
-    db = client.db('Testdb');
-  }
-  catch(error){
+    console.log("connected");
+    let db = client.db("blockTechDB");
+  } catch (error) {
     console.error(error);
     throw error;
   }
 };
 
+app.post("/account", async (req, res) => {
+  connectDB();
+
+  const user = {
+    slug: slug(req.body.name + req.body.lastname, { lower: false }),
+    name: req.body.name,
+    lastname: req.body.lastname,
+    dateofbirth: req.body.dateofbirth,
+    gender: req.body.gender,
+    email: req.body.email,
+    password: req.body.password,
+    passwordcheck: req.body.passwordcheck
+  };
+  if (req.body.password !== req.body.passwordcheck) {
+    res.send("Passwords do not match");
+  } else {
+    db.collection("collection1").insertOne(user, function (err, collection) {
+      if (err) throw err;
+      console.log("succes");
+    });
+    res.render("account", { user });
+  }
+});
 
 app.listen(port, async () => {
   console.log(chalk.green(`My new first server hosted on port ${port}!`));
-  let databaseConnection = await connectDB();
-  let theData = await db.collection('collection1').find({}).toArray();
+  const databaseConnection = await connectDB();
+  const theData = await db.collection("collection1").find({}).toArray();
+  // await db.collection('collection1').deleteMany({});     TO DELETE DB DATA
 });
 
-
-app.post('/userdetails', async (req, res) => {
-  connectDB();
-
-  const userdata = {
-    name: req.body.name,
-    age: req.body.age,
-    gender: req.body.gender,
-    genres: req.body.genres
-  };
-  db.collection('collection1').insertOne(userdata,function(err, collection)
-  {if (err) throw err;
-    console.log("succes");
-  })
-  res.send(userdata);
+// 404 PAGE
+app.use((req, res) => {
+  res.status(404).render("404");
 });
+
+// REMOVE ACCOUNT
