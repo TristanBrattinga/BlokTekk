6e210e5f6e14f5e2d10c7950e35babf250a700b5
Tristan Brattinga <tristan.brattinga@gmail.com>
2023-04-10 13:55:36 +0200

    DEL: moved or deleted

--- public/js/script.js
+++
@@ -1,9 +0,0 @@
-const burgerBtn = document.querySelector("header nav button");
-const mobileNav = document.querySelector("header nav ul");
-
-burgerBtn.addEventListener("click", () => {
-    burgerBtn.classList.toggle("showMenu");
-    mobileNav.classList.toggle("showMenu");
-
-
-});
